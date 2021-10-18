2f768232af8f35a9b0f3549c8e23a214d7785262
andrew morales <andrew.morales725@gmail.com>
2021-10-18 15:01:29 -0400

    Updated player turn methods

--- play_field.py
+++ play_field.py
@@ -1,4 +1,5 @@
 from player import Player
+from string import ascii_uppercase
 
 class Play_Field:
 
@@ -7,11 +8,84 @@ class Play_Field:
         self.player_two = Player()
         self.run_game()
 
+    def letters_to_numbers(self, location):
+        letters_to_numbers = {}
+
+        for count in range(1,21):
+            letters_to_numbers[ascii_uppercase[count - 1]] = count
+
+        location = list(location)
+        
+        if len(location) > 2:
+            location[1] = f'{location[1]}{location[2]}'
+        
+        location[0] = letters_to_numbers[location[0].upper()]
+        location[1] = int(location[1])
+
+        return location
+
     def player_one_turn(self):
-        pass
+        print(f"{self.player_one.name}'s turn!")
+        input('Press enter to continue: ')
+
+        self.player_two.game_board.show_board()
+        attack = input('Enter a location ie "A1" to attack: ')
+
+        converted_location = self.letters_to_numbers(attack)
+        row = converted_location[0]
+        column = converted_location[1]
+
+        if self.player_two.hidden_board[row][column] == '[ ]':
+            self.player_two.hidden_board[row][column] = '[O]'
+            print('Attack missed!')
+
+        else:
+            ship_hit = self.player_two.hidden_board[row][column]
+            for ship in self.player_two.ships:
+                if ship.symbol == ship_hit[1]:
+                    ship.size -= 1
+                    if ship.size == 0:
+                        self.player_two.ships.remove(ship)
+                        print(f"You sunk {self.player_two.name}'s {ship.name}")
+                    else:
+                        print(f"You hit {self.player_two.name}'s {ship.name}")
+                    
+            self.player_two.hidden_board[row][column] = '[X]'
+            self.player_two.game_board[row][column] = '[X]'
+            self.player_two.game_board.show_board()
+
 
     def player_two_turn(self):
-        pass
+        print(f"{self.player_two.name}'s turn!")
+        input('Press enter to continue: ')
+
+        self.player_one.game_board.show_board()
+        attack = input('Enter a location ie "A1" to attack: ')
+
+        converted_location = self.letters_to_numbers(attack)
+        row = converted_location[0]
+        column = converted_location[1]
+
+        if self.player_one.hidden_board[row][column] == '[ ]':
+            self.player_one.hidden_board[row][column] = '[O]'
+            self.player_one.game_board[row][column] = '[O]'
+            print('Attack missed!')
+
+        else:
+            ship_hit = self.player_one.hidden_board[row][column]
+            for ship in self.player_one.ships:
+                if ship.symbol == ship_hit[1]:
+                    ship.size -= 1
+                    if ship.size == 0:
+                        self.player_one.ships.remove(ship)
+                        print(f"You sunk {self.player_one.name}'s {ship.name}")
+                    else:
+                        print(f"You hit {self.player_one.name}'s {ship.name}")
+                    
+            self.player_one.hidden_board[row][column] = '[X]'
+            self.player_one.game_board[row][column] = '[X]'
+    
+        self.player_one.game_board.show_board()
 
     def round_of_play(self):
         self.player_one_turn()
