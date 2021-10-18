4ff4bd1b80cf65936c1c86c1893d09d70cb13c63
andrew morales <andrew.morales725@gmail.com>
2021-10-18 13:51:28 -0400

    Updated game board __init__

--- game_board.py
+++ game_board.py
@@ -3,26 +3,27 @@ from string import ascii_uppercase
 class Game_Board:
     
     def __init__(self):
+        self.game_board = []
         self.create_board()
 
     def create_board(self):
-        game_board = [['[ ]' for columns in range(21)]for row in range(21)]
+        self.game_board = [['[ ]' for columns in range(21)]for row in range(21)]
 
-        game_board[0][0] = ' '
+        self.game_board[0][0] = ' '
 
         for count in range(1,21):
             char = ascii_uppercase[count - 1]
-            game_board[count][0] = char
+            self.game_board[count][0] = char
 
         for count in range(1,10):
-            game_board[0][count] = f' {count} '
+            self.game_board[0][count] = f' {count} '
 
         for count in range(10,21):
-            game_board[0][count] = f'{count} '
+            self.game_board[0][count] = f'{count} '
 
-    def show_board(self, board_name):
+    def show_board(self):
 
-        for rows in board_name:
+        for rows in self.game_board:
             for element in rows:
                 print(element, end = ' ')
             print('')
