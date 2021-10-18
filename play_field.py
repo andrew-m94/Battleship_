d98a6b794c1e0828b5a32aca8f0145c41551e943
andrew morales <andrew.morales725@gmail.com>
2021-10-18 12:42:48 -0400

    Updated method structure

--- play_field.py
+++ play_field.py
@@ -4,6 +4,7 @@ class Play_Field:
 
     def __init__(self):
         self.player = Player()
+        self.run_game()
 
     def player_one_turn(self):
         pass
@@ -11,8 +12,15 @@ class Play_Field:
     def player_two_turn(self):
         pass
 
+    def round_of_play(self):
+        self.player_one_turn()
+        self.player_two_turn()
+
     def run_game(self):
-        pass
+        self.intro()
+        self.player.place_ships()
+        self.round_of_play()
+        self.display_winner()
 
     def intro(self):
         pass
