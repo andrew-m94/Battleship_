3307ef508266a538f98b89ab8ba8c4b32c8bd4d9
andrew morales <andrew.morales725@gmail.com>
2021-10-19 13:38:31 -0400

    Added methods for selecting inputs

--- ai.py
+++ ai.py
@@ -1,4 +1,5 @@
 from player import Player
+import random
 
 class Ai(Player):
     def __init__(self, name):
@@ -7,3 +8,16 @@ class Ai(Player):
 
     def place_ships(self):
         pass
+
+    def auto_array_pick(self):
+        auto_array_options = self.generate_user_options()
+        max_range = len(auto_array_options)
+        rand_num = random.randrange(0,max_range)
+        rand_input = auto_array_options[rand_num]
+
+        return rand_input
+
+    def auto_orientation(self):
+        orientation = random.randrange(1,3)
+
+        return orientation
